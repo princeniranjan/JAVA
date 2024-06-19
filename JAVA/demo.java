270e63ba3582c972e3a1f4b86c265266464f834c
princeniranjan <niranjanniranjan2585@gmail.com>
2024-06-19 20:12:16 +0530

    Add files via upload
    HERE YOU CAN REFER THE BASIC CONCEPTS OF JAVA

---
+++ JAVA/demo.java
@@ -0,0 +1,19 @@
+class demo
+{
+    public static void main(String[] args)
+     {
+        int x = 20;
+
+        if ((x>=20)) 
+        {
+              System.out.println("hello");
+              System.out.print("hi");
+
+
+        }
+
+        else
+        System.out.print("bye");
+            
+        }
+ }
