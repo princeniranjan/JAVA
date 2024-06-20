1edc575aa0c26139e3819749cc6fdc1ca77eb986
princeniranjan <niranjanniranjan2585@gmail.com>
2024-06-20 12:03:51 +0530

    Add files via upload

---
+++ switching.java
@@ -0,0 +1,34 @@
+public class switching
+{
+    public static void main(String args[])
+    {
+        int n = 9;
+        switch (n) {
+            case 1:
+                System.out.print("monday");
+                break;
+            case 2:
+                System.out.print("Tuesday");
+                break;
+            case 3 :
+                System.out.print("Wednesday");  
+                break;
+            case 4:
+                System.out.print("Thursday");  
+                break;
+            case 5:
+                System.out.print("Friday");
+                break;
+            case 6:
+                System.out.print("Saturday");
+                break;
+            case 7:
+                System.out.print("Sunday");
+                break;
+            default:
+                System.out.print("Enter a Valid Number");
+
+        }
+
+    }
+}
