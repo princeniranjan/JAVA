44526e75f86f6444097372281e58c9d04a8a4026
princeniranjan <niranjanniranjan2585@gmail.com>
2024-06-21 18:15:37 +0530

    Add files via upload

---
+++ JAVA/forloop.java
@@ -0,0 +1,10 @@
+public class forloop 
+{
+    public static void main(String a[])
+    {
+        for(int i = 1;i <= 5;i++)
+        {
+            System.out.println("HI " +i );
+        }
+    }
+}
---
+++ JAVA/whileloop.java
@@ -0,0 +1,15 @@
+public class whileloop 
+{
+    public static void main(String a[])
+    {
+        int i = 1;
+        while (i<=5)
+        {
+            System.out.println("HI " + i );
+            i++;
+        }
+        System.out.print("Bye " + i);
+    }
+
+    
+}
