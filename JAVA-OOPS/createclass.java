25feafc90e73bc1c0b04a800e9dbbea568752543
princeniranjan <niranjanniranjan2585@gmail.com>
2024-06-23 08:57:15 +0530

    Add files via upload

---
+++ JAVA-OOPS/createclass.java
@@ -0,0 +1,16 @@
+//creating the class 
+class calculator
+{
+    //cretaing the variable 
+    int a ;//variable is created inside the class and outside the method
+
+
+    // public- used to access from anywhere
+    // second we have to initialise the type (return )
+   // third we have to create a function/method
+    public int add( int n1 , int n2)
+    {
+       int r = n1+n2;
+       return r;
+    }
+}
