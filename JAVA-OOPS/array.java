d32dbaa5c42ae3134c48bf20c05ee0cca3ba0cde
princeniranjan <niranjanniranjan2585@gmail.com>
2024-06-27 15:59:17 +0530

    Add files via upload

---
+++ JAVA-OOPS/array.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class arrays
+{
+  public static void main(String a[])
+  {
+    int nums[] = {3,4,5,6,7};
+   // System.out.print(nums);
+   System.out.println(nums[1]);
+   System.out.println(Arrays.toString(nums));//to print all the values int he array
+     
+  }  
+}
+// System.out.print(nums); attempts to print an array directly. In Java, when you try to print an array using System.out.print,
+// it prints the type and the hash code of the array rather than its contents.
+
+// To print the elements of the array, you need to loop through the array or use a method from the Arrays class
+
+
+
+
---
+++ JAVA-OOPS/array1.java
@@ -0,0 +1,13 @@
+public class arrays1
+{
+  public static void main(String a[])
+  {
+    int nums [] = new int[4];
+    nums[0] = 1;
+    nums[1] = 2;
+    nums[2] = 3;
+    nums[3] = 4;
+    for(int i =0 ; i<=3; i++)
+    System.out.println(nums[i]);
+  }  
+}
